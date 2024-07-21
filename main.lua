1547cb95d21109d85ab219233815c3c561a24474
anny <annyman.81194@protonmail.com>
2024-07-21 22:50:10 +0530

    initial template!!

--- main.lua
+++ main.lua
@@ -2,7 +2,19 @@ function love.load()
     love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
 end
 
+function love.update(dt)
+    --
+end
+
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.print("Welcome to Mooncrypt!", 400, 300)
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 end
+
+-- exit program press 'Q'
+function love.keypressed(k)
+    if k == 'q' then
+       love.event.quit()
+    end
+ end
