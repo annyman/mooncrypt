29cb438c4bbf22980227a647fd71cd1fc6dafa6f
anny <annyman.81194@protonmail.com>
2024-07-21 22:47:05 +0530

    Initial commit with main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+function love.load()
+    love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
+end
+
+function love.draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Welcome to Mooncrypt!", 400, 300)
+end
